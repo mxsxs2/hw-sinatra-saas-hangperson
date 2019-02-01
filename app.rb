e141f5ee4e829f7fc48aabbd2458df608ec57c54
Mxsxs2 <>
2019-02-01 14:21:10 +0000

    Change validation messages

--- app.rb
+++ app.rb
@@ -40,9 +40,9 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     begin
-      session[:usedletter] = @game.guess(letter)
+      session[:flash_message] = @game.guess(letter) ? '' : 'You have already used that letter.'
     rescue ArgumentError
-      session[:usedletter] = true
+      session[:flash_message] = 'Invalued guess.'
     end
     redirect '/show'
   end
@@ -53,7 +53,8 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    @usedletter = session[:usedletter]
+    session[:flash_message] = '' if session[:flash_message].nil?
+    @flash_message = session[:flash_message]
     redirect '/' if @game.word.nil? || @game.word.empty?
     redirect '/win' if @game.check_win_or_lose == :win
     redirect '/lose' if @game.check_win_or_lose == :lose
